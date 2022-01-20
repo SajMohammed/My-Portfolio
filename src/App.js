9f6ecccefa75dd478773914fc6edc8e372b0fe92
SajMo <mail2sajmo@gmail.com>
2022-01-21 00:29:02 +0530

    Added the file structure and basic css properties in index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import { Blog, Features, Footer, Header} from './containers';
+import { Navbar, Article, Feature } from './components';
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <div className="gradient__bg">
+        <Navbar />  
+        <Header />
+      </div>
+      <Features />
+      <Blog />
+      <Footer />
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './navbar.css';
 
 const Navbar = () => {
-  return <div></div>;
+  return <div>
+    Navbar
+  </div>;
 };
 
 export default Navbar;
