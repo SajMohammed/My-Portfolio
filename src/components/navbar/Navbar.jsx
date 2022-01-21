7a4d8fef1eb5a8e30e3c1c01b5ac29aacff3443d
SajMo <mail2sajmo@gmail.com>
2022-01-21 16:55:23 +0530

    Navbar skeleton done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
-  return <div>
-    Navbar
-  </div>;
+  return (
+  <div className="portfolio__navbar">
+    <div className="portfolio__navbar-links">
+      <div className="portfolio__navbar-links_logo">
+        <h1>SAJMO</h1>
+      </div>
+      <div className="portfolio__navbar-links_container">
+        <p><a href="#home">Home</a></p>
+        <p><a href="#about">About</a></p>
+        <p><a href="#blog">Blog</a></p>
+        <p><a href="#connect">Connect</a></p>
+      </div>
+    </div>
+  </div>
+  )
 };
 
 export default Navbar;
