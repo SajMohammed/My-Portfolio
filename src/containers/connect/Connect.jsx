a62065f70380d23b86dda03bbd0ae49b85eb7160
SajMo <mail2sajmo@gmail.com>
2022-01-24 00:17:10 +0530

    done the connect section, added social icons and links, responsiveness done

--- src/containers/connect/Connect.jsx
+++ src/containers/connect/Connect.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './connect.css';
-import connectImage from '../../assets/Sally-4.svg';
+import {connectImage, linkedin, github, twitter, gmail, instagram} from './imports';
 
 const Connect = () => {
   return (
     <div className="folio__connect section__padding" id="connect">
         <div className="folio__connect-content">
-            <h1 className="gradient__text"> The Possibilities are beyond Imagination</h1>
+            <h1 className="gradient__text">Connect with me on my Socials.<br/> Let's build something amazing together</h1>
+            <div>
+
+                <div>
+                    <a href="https://www.linkedin.com/in/saj-s/" target="_blank"><img src={linkedin} alt="Linkedin"/></a>
+                </div>
+                <div>
+                    <a href="https://github.com/SajMohammed" target="_blank"><img src={github} alt="github"/></a>
+                </div>
+                <div>
+                    <a href="https://twitter.com/SajMohammed" target="_blank"><img src={twitter} alt="twitter"/></a>
+                </div>
+                <div>
+                    <a href="mailto:sajmohammed.s@gmail.com" target="_blank"><img src={gmail} alt="gmail"/></a>
+                </div>
+                <div>
+                    <a href="https://www.instagram.com/the_hungry_bedouin/" target="_blank"><img src={instagram} alt="instagram"/></a>
+                </div>
+            </div>
         </div>
         <div className="folio__connect-image">
             <img src={connectImage} alt="connect" />
---
+++ src/containers/connect/imports.js
@@ -0,0 +1,8 @@
+import connectImage from '../../assets/Saly-19.svg'
+import linkedin from '../../assets/LinkedIN.svg';
+import github from '../../assets/Github.svg';
+import twitter from '../../assets/Twitter.svg';
+import gmail from '../../assets/Gmail.svg';
+import instagram from '../../assets/Instagram.svg';
+
+export { connectImage, linkedin, github, twitter, gmail, instagram }
