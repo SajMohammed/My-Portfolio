6b223de5009dd92fb027bf73e2a12dc6fe2d9cbb
SajMo <mail2sajmo@gmail.com>
2022-01-30 11:25:58 +0530

    Resume component

---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './resume.css';
+
+const Resume = () => {
+  return (
+    <div className="folio__resume">
+        <div className="folio__resume-content">
+            <h1>Click the download button for my resume</h1>
+        </div>
+        <div className="folio__resume-button">
+            <button type="button">Download</button>
+        </div>
+    </div>
+  )};
+
+export default Resume;
