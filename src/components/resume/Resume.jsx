bf239d5dd390d630c0ce96cdcd73cee2c07661d0
SajMo <mail2sajmo@gmail.com>
2022-01-30 13:33:06 +0530

    completed Resume download banner

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './resume.css';
+import resume from '../../assets/Saj_Resume.pdf';
 
 const Resume = () => {
   return (
     <div className="folio__resume">
         <div className="folio__resume-content">
-            <h1>Click the download button for my resume</h1>
+            <h2>Click the download button for my resume</h2>
         </div>
         <div className="folio__resume-button">
-            <button type="button">Download</button>
+            <a href={resume} download><button type="button">Download</button></a>
         </div>
     </div>
   )};
