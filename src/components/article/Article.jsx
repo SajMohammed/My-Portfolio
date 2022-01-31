6d6f2d2b0aea90b8b9e5d93ad75128f72ea36300
SajMo <mail2sajmo@gmail.com>
2022-01-31 17:22:26 +0530

    Added blog component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({ imageURL, date, title }) => {
   return (
-    <div>
-        Article
+    <div className="folio__blog-container_article">
+        <div className="folio__blog-container_article-image">
+          <img src={imageURL} alt="blog image"/>
+        </div>
+        <div className="folio__blog-container_article-content">
+          <div>
+            <p>{date}</p>
+            <h3>{title}</h3>
+            <p>Read Full Article</p>
+          </div>
+        </div>
     </div>
   )
 };
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Article } from '../../components'
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 import './blog.css';
 
 const Blog = () => {
-  return <div>
-      Blog
-  </div>;
-};
+  return (
+    <div className="folio__blog section__padding" id="blog">
+      <div className="folio__blog-heading">
+        <h1 className="gradient__text">A lot is happening. I am blogging about it</h1>
+      </div>
+      <div className="folio__blog-container">
+        <div className="folio__blog-container_groupA">
+          <Article imageURL={blog01} date="Jan 31, 2022" title="Blockchain and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+        <div className="folio__blog-container_groupB">
+          <Article imageURL={blog02} date="Jan 31, 2022" title="Blockchain and Open  AI is the future. Let us exlore how it is?" />
+          <Article imageURL={blog03} date="Jan 31, 2022" title="Blockchain and Open  AI is the future. Let us exlore how it is?" />
+          <Article imageURL={blog04} date="Jan 31, 2022" title="Blockchain and Open  AI is the future. Let us exlore how it is?" />
+          <Article imageURL={blog05} date="Jan 31, 2022" title="Blockchain and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+        
+      </div>
+        
+    </div>
+  )};
 
 export default Blog;
